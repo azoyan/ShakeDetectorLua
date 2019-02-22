40e4675bf1c09f55edc354c43fc90d60b3b3df0a
Ivan Azoyan <azoyan.89@mail.ru>
2019-02-22 15:09:30 +0300

    refactoring
    only field "count"  accessible outside

--- shakeDetector.lua
+++ shakeDetector.lua
@@ -1,10 +1,9 @@
-local ShakeDetector = {}
-local lastX, lastY, lastZ, timer, lastTime = 0, 0, 0, 0, 0
+local ShakeDetector = { count = 0 }
+local lastX, lastY, lastZ, timer, lastTime, threshold, timeout = 0, 0, 0, 0, 0, 0.5, 0.25
 
-function ShakeDetector:init(threshold, timeout)
-    self.th = threshold or 0.5
-    self.timeout = timeout or 0.25
-    self.count = 0
+function ShakeDetector:reset(threshold, timeout)
+    theshold = threshold or 0.5
+    timeout = timeout or 0.25
 end
 
 function ShakeDetector:update(dt, x, y, z)
@@ -12,8 +11,8 @@ function ShakeDetector:update(dt, x, y, z)
     if lastX == 0 and lastY == 0 and lastZ == 0 then lastX, lastY, lastZ = x, y, z end
     local dx, dy, dz = math.abs(lastX - x), math.abs(lastY - y), math.abs(lastZ - z)
 
-    if dx > self.th and dy > self.th or dx > self.th and dz > self.th or dy > self.th and dz > self.th then
-        if timer - lastTime > self.timeout then
+    if dx > threshold and dy > threshold or dx > threshold and dz > threshold or dy > threshold and dz > threshold then
+        if timer - lastTime > timeout then
             lastTime = timer
             self.count = self.count + 1
         end
